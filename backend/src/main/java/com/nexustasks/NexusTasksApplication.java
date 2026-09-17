b28596ba16f49abf52dbb0ca796721d000171585
Lalegende21 <delfredtene17@gmail.com>
2026-09-17 15:55:26 +0100

    chore(backend): initialize spring boot project, multi-profile configs and local docker db

---
+++ backend/src/main/java/com/nexustasks/NexusTasksApplication.java
@@ -0,0 +1,12 @@
+package com.nexustasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusTasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexusTasksApplication.class, args);
+    }
+}
